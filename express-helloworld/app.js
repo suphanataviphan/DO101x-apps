8c343899ecf210ef2389eea5f4e85c44e2b55824
Suphanat Aviphan <suphanat.aviphan@infologistix.de>
2023-02-22 11:16:21 +0100

    add /mars endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,6 @@ app.get('/mars', function(req, res) {
   res.send('Hello Mars!\n');
   });
 
-app.listen(8080, function () {
+app.listen(3000, function () {
   console.log('Example app listening on port 8080!');
 });
